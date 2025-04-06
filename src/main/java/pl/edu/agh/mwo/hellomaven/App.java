822af58ac8ba5a65e3547828948a902194a33d72
Andrij Kostecki <kostecki.andrij@gmail.com>
2025-04-06 14:45:18 +0200

    First changes

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,17 +1,36 @@
 package pl.edu.agh.mwo.hellomaven;
 
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
+
 /**
- * Simple main application
+ * Simple main application.
  */
 public class App {
 
+
+    public static final int width = 120;
+    public static final int height = 20;
+    public static final int INT = 4;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        IRender render = new Render();
+        IContextBuilder builder = render.newBuilder();
+        builder.width(width).height(height);
+        builder.element(new PseudoText("Hello World!"));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
+        //System.out.println("Hello World!");
     }
     
     public int getRandomNumber() {
         // chosen by fair dice roll.
         // guaranteed to be random
-        return 4;
+        return INT;
     }
 }
